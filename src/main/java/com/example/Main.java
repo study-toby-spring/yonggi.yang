d4b128a1b8fdfbbde298ca3a8cc8d815a96e24f9
양용기 <brave0681@gmail.com>
2017-10-10 18:02:33 +0900

    [1.5]스프링의 IoC

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,13 +5,15 @@ import com.example.dao.DConnectionMaker;
 import com.example.dao.DaoFactory;
 import com.example.dao.UserDao;
 import com.example.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
         User user = dao.get("1");
         System.out.println(user.getName());
         System.out.println(user.getPassword());
--- src/main/java/com/example/dao/DaoFactory.java
+++ src/main/java/com/example/dao/DaoFactory.java
@@ -1,10 +1,18 @@
 package com.example.dao;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+
+    @Bean
     public UserDao userDao(){
-        UserDao userDao = new UserDao(connectionMaker());
-        return userDao;
+        return new UserDao(connectionMaker());
     }
+
+    @Bean
     public ConnectionMaker connectionMaker(){
         return new DConnectionMaker();
     }
