8e646dcafc8b27f2a995eefff2668ce02d885876
양용기 <brave0681@gmail.com>
2017-10-10 18:07:45 +0900

    [1.6]싱글톤 레지스트리와 오브젝트 스코프

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -12,13 +12,20 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        User user = dao.get("1");
-        System.out.println(user.getName());
-        System.out.println(user.getPassword());
+        DaoFactory factory = new DaoFactory();
+        UserDao dao1 = factory.userDao();
+        UserDao dao2 = factory.userDao();
 
-        System.out.println(user.getId() + "조회 성공");
+        System.out.println(dao1);
+        System.out.println(dao2);
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao3 = context.getBean("userDao", UserDao.class);
+        UserDao dao4 = context.getBean("userDao", UserDao.class);
+
+        System.out.println(dao3);
+        System.out.println(dao4);
+
 
     }
 }
--- src/main/java/com/example/dao/UserDao.java
+++ src/main/java/com/example/dao/UserDao.java
@@ -6,8 +6,9 @@ import java.sql.*;
 
 public class UserDao {
 
-
     private ConnectionMaker connectionMaker;
+    private Connection c;
+    private User user;
 
     public UserDao(ConnectionMaker connectionMaker){
         this.connectionMaker = connectionMaker;
@@ -15,7 +16,7 @@ public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
 
-        Connection c = connectionMaker.makeConnection();
+        c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -29,14 +30,14 @@ public class UserDao {
 
     public User get(String id) throws ClassNotFoundException, SQLException {
 
-        Connection c = connectionMaker.makeConnection();
+        c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
         rs.next();
 
-        User user = new User();
+        user = new User();
         user.setId(rs.getString("id"));
         user.setName(rs.getString("name"));
         user.setPassword(rs.getString("password"));
