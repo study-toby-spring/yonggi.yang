edff86f5a10c53af3efc0cefa93481fe36a38b27
양용기 <brave0681@gmail.com>
2017-10-14 16:58:32 +0900

    [3.1]다시보는 초난감 DAO

--- src/main/java/com/example/dao/UserDao.java
+++ src/main/java/com/example/dao/UserDao.java
@@ -56,27 +56,73 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
-        ps.close();
-        c.close();
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null){
+                try{
+                    ps.close();
+
+                } catch(SQLException e){
+
+                }
+            }
+            if (c != null){
+                try{
+                    c.close();
+
+                } catch(SQLException e){
+
+                }
+            }
+
+        }
     }
 
     public int getCount() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("select count(*) from users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (rs != null){
+                try{
+                    rs.close();
 
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+                } catch(SQLException e){
 
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
+                }
+            }
+            if (ps != null){
+                try{
+                    ps.close();
 
-        rs.close();
-        ps.close();
-        c.close();
+                } catch(SQLException e){
 
-        return count;
+                }
+            }
+            if (c != null){
+                try{
+                    c.close();
+
+                } catch(SQLException e){
+
+                }
+            }
+        }
     }
 
 }
